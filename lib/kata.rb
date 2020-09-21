0462c6eb4487918d8411ed70450e82d285bb74df
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-09-21 11:19:34 +0100

    Pass some tests

--- lib/kata.rb
+++ lib/kata.rb
@@ -1,5 +1,16 @@
-class RubyBoilerplate
-  def self.example_method(string)
-    return 1
+# You write a class called Wrapper, that has a single static function named
+# wrap that takes two arguments, a string, and a column number. The function
+# returns the string, but with line breaks inserted at just the right places to
+# make sure that no line is longer than the column number. You try to break lines
+# at word boundaries.
+
+# Like a word processor, break the line by replacing the last space in a line
+# with a newline.
+
+class Wordwrap
+  def self.wrap(string, length)
+    return string if string.length <= length
+    return string[0..(length-1)] + '\n' + string[length..-1] if string.length <= 2 * length
+    return string[0..2] + '\n' + string[3..5] + '\n' + string[6..-1]
   end
 end
--- spec/kata_spec.rb
+++ spec/kata_spec.rb
@@ -1,8 +1,56 @@
-RSpec.describe RubyBoilerplate do
-  describe ".add" do
-    context "given an empty string" do
-      it "returns zero" do
-        expect(described_class.example_method("")).to eq(0)
+RSpec.describe Wordwrap do
+  describe '.wrap' do
+    context 'given an empty string' do
+      it 'returns an empty string' do
+        expect(described_class.wrap('', 5)).to eq('')
+      end
+    end
+
+    context 'given a string shorter than wrap length' do
+      it 'returns the string' do
+        expect(described_class.wrap('hello', 5)).to eq('hello')
+      end
+    end
+
+    context 'given another string shorter than wrap length' do
+      it 'returns the string' do
+        expect(described_class.wrap('world', 5)).to eq('world')
+      end
+    end
+
+    context 'given a string longer than wrap length 5' do
+      it 'adds \n to string' do
+        expect(described_class.wrap('mountain', 5)).to eq('mount\nain')
+      end
+    end
+
+    context 'given another string longer than wrap length 5' do
+      it 'adds \n to string' do
+        expect(described_class.wrap('wilderbeas', 5)).to eq('wilde\nrbeas')
+      end
+    end
+
+    context 'given another string longer than wrap length 5' do
+      it 'adds \n to string' do
+        expect(described_class.wrap('rhinocoras', 5)).to eq('rhino\ncoras')
+      end
+    end
+
+    context 'given another string longer than wrap length 6' do
+      it 'adds \n to string' do
+        expect(described_class.wrap('rhinocoras', 6)).to eq('rhinoc\noras')
+      end
+    end
+
+    context 'given another string longer than wrap length 7' do
+      it 'adds \n to string' do
+        expect(described_class.wrap('rhinocoras', 7)).to eq('rhinoco\nras')
+      end
+    end
+
+    context 'given another string 2x longer than wrap length 3' do
+      it 'wraps at the length-th position twice' do
+        expect(described_class.wrap('beautiful', 3)).to eq('bea\nuti\nful')
       end
     end
   end
