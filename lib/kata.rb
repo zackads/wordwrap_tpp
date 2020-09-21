7a0240fde02672b43c910a95fb7ed82aa32c0d79
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-09-21 15:21:39 +0100

    Add recursive transformation

--- lib/kata.rb
+++ lib/kata.rb
@@ -9,16 +9,11 @@
 
 class Wordwrap
   def self.wrap(string, length)
-    return string if string.length <= length
-    return string[0..(length-1)] + '\n' + string[length..-1] if string.length <= 2 * length
-    return string[0..(length-1)] + '\n' + string[length..(2 * length - 1)] + '\n' + string[(2 * length)..-1] if string.length <= 3 * length
-    
-    output = string[0..(1*length-1)]
-    for i in 2..4 do 
-      output << '\n' + string[(i-1)*length..((i*length)-1)]
-      
+    if string.length <= length
+      string
+    else
+      new_string = string[length..-1]
+      string[0..(length - 1)] + '\n' + wrap(new_string, length)
     end
-    
-    return output 
   end
 end
